8f5a1a6cf38e4726ab32270e031f0b17cde65ff3
Nur Islam <nurislam0333@gmail.com>
2018-02-16 00:52:20 +0600

    redux started

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
 import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
@@ -10,9 +13,11 @@ import 'bootstrap/dist/css/bootstrap.css'; // bootstrap CSS
 
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
   ),
   document.getElementById('root')
 );
